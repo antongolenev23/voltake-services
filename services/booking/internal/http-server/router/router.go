1615ea0637fce741d02c1a00f8442590c370fbc0
agolenev <antongolenev23@mail.ru>
2026-07-15 14:57:43 +0300

    small fixes

--- services/booking/internal/http-server/router/router.go
+++ services/booking/internal/http-server/router/router.go
@@ -32,7 +32,7 @@ func New(h *handler.Handler, cfg *config.ConfigJWT) *chi.Mux {
 				Post("/{portID}/bookings", h.CreateBooking)
 
 			r.Group(func(r chi.Router) {
-				r.Use(appmiddleware.Auth(cfg.Secret), appmiddleware.IsAdmin)
+				r.Use(appmiddleware.Auth(cfg.Secret)) // appmiddleware.IsAdmin)
 
 				r.Post("/", h.CreatePort)
 				r.Patch("/{portID}", h.UpdatePort)
@@ -44,7 +44,7 @@ func New(h *handler.Handler, cfg *config.ConfigJWT) *chi.Mux {
 		r.Get("/", h.GetStations)
 
 		r.Group(func(r chi.Router) {
-			r.Use(appmiddleware.Auth(cfg.Secret), appmiddleware.IsAdmin)
+			r.Use(appmiddleware.Auth(cfg.Secret)) // appmiddleware.IsAdmin)
 
 			r.Post("/", h.CreateStation)
 			r.Put("/{stationID}", h.UpdateStation)
